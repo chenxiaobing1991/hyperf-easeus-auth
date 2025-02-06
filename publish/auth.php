bcdf9cb0bc2f0a6016f49880cf5a9b034fa73bd4
chenxiaobing <chenxiaobing@namek-game.com>
2025-02-06 10:17:09 +0800

    4

--- publish/auth.php
+++ publish/auth.php
@@ -1,30 +1,18 @@
 <?php
 
 return [
-    'enable' => (bool)env('AUTH_SSO_ENABLED', true),
-    'guard' => env('AUTH_SSO_GUARD', 'default'),
+    'default' => [
+        'guard' => 'default',
+        'driver' => 'default',
+    ],
     'guards' => [
         'default' => [
-            'driver' => \Cxb\Hyperf\Easeus\Auth\Driver\DefaultDriver::class,
-            'address' => env('AUTH_SSO_ADDRESS', 'http://account-center-dev.sit.easeus.com/micro-users'),
-//            'username' => 'nacos',
-//            'password' => 'nacos',
-        ],
+            'uri' => 'http://192.168.0.9:8089',//地址
+        ]
     ],
-//    'guards' => [
-//        'dev' => [
-//            'uri' => 'http://192.168.0.9:8089',//开发
-//        ],
-//        'test' => [
-//            'uri' => 'http://account-center-dev.sit.easeus.com/micro-users',//测试
-//        ],
-//        'prod' => [
-//            'uri' => 'http://account-center.sit.easeus.com/micro-users',//生产
-//        ],
-//    ],
-//    'drivers' => [
-//        'default' => [
-//            'driver' => \App\Component\Admin\IdentityManager::class,
-//        ]
-//    ]
+    'drivers' => [
+        'default' => [
+            'driver' => \App\Component\Admin\IdentityManager::class,
+        ]
+    ]
 ];
